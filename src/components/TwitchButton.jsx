63f426548a669fb8acc80d02538a30194adce4ec
Wei Lun <garyganweilun@gmail.com>
2022-01-27 01:40:11 +0800

    Register page done

---
+++ src/components/TwitchButton.jsx
@@ -0,0 +1,13 @@
+import {createButton} from "react-social-login-buttons";
+
+
+const config = {
+  text: "Login with Twitch",
+  icon: "twitch",
+  iconFormat: name => `fa fa-${name}`,
+  style: { background: "#9146FF" },
+};
+
+const MyTwitchLoginButton = createButton(config);
+
+export default MyTwitchLoginButton;
