5b1cd2b615759d31e0ef423958a93eef61821275
Wei Lun <garyganweilun@gmail.com>
2022-01-28 18:07:58 +0800

    Able to login and go into home page

--- craco.config.js
+++ craco.config.js
@@ -10,9 +10,10 @@ module.exports = {
             modifyVars: {
                 '@primary-color': '#6441a5',
                 '@heading-color': '#262626',
-                '@layout-header-background': '#b9a3e3',
+                '@layout-header-background': '#E5E5E5',
                 '@layout-body-background': '#b9a3e3',
                 '@layout-footer-background': '#FFFFFF',
+                '@layout-sider-background': '#b9a3e3'
             },
             javascriptEnabled: true,
           },
--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
-import { Route, Routes } from "react-router-dom";
+import React, { useContext } from 'react';
+import { Route, Routes, Navigate } from "react-router-dom";
 import './App.less';
+import { AuthContext } from './context/AuthContextProvider';
 import Faq from './pages/Faq';
+import Home from './pages/Home';
 import Register from './pages/Register';
 
-const App = () => (
-  <div className="App">
+const App = () => {
+
+  const userSession = useContext(AuthContext)
+  console.log(userSession)
+
+  return (<div className="App">
     <Routes>
-      <Route path="/" element={<Register />} />
-      <Route path="/faq" element={<Faq />} />
+      {userSession ? <>
+        <Route path="*" element={<Navigate to="/home" />}  />
+        <Route path="/home" element={<Home />} />
+      </> :
+
+        <>
+          <Route path="*" element={<Navigate to="/" />}  />
+          <Route path="/" element={<Register />} />
+          <Route path="/faq" element={<Faq />} />
+        </>}
+
     </Routes>
   </div>
-);
+  );
+}
+
 
 export default App;
