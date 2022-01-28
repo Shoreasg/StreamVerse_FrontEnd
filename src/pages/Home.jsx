f40ec1db0f7df95c616bf3a8e44447a0de18e0c2
Wei Lun <garyganweilun@gmail.com>
2022-01-29 03:32:01 +0800

    Added logo,search,logout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Layout, Col, Row } from 'antd';
-
+import { Layout, Col, Row, Input } from 'antd';
+import logo from "../logo/Streamverselogo.png"
 const { Footer, Content, Header, Sider } = Layout;
+const { Search, TextArea } = Input;
+
 
 
 
@@ -10,17 +12,20 @@ const Home = () => {
         <Layout>
             <Header>
                 <Row>
-                    <Col span={8}>logo be here</Col>
-                    <Col span={8} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>search bar goes here</Col>
-                    <Col span={8} style={{ textAlign: "right" }}>Logout</Col>
+                    <Col xxl={{ span: 8 }} xl={{ span: 8 }} lg={{ span: 8 }} md={{ span: 8 }} sm={{ span: 8 }} xs={{ span: 8 }} ><img src={logo} width={"90"} style={{ position: "absolute", top: -10, left: -30 }} alt="Logo"></img></Col>
+                    <Col xxl={{ span: 8 }} xl={{ span: 8 }} lg={{ span: 8 }} md={{ span: 8 }} sm={{ span: 8 }} xs={{ span: 8 }} ><Search placeholder="input search text" enterButton style={{ position: "absolute", top: 20 }} /></Col>
+                    <Col xxl={{ span: 8 }} xl={{ span: 8 }} lg={{ span: 8 }} md={{ span: 8 }} sm={{ span: 8 }} xs={{ span: 8 }} style={{ textAlign: "right" }}>Logout</Col>
                 </Row>
             </Header>
             <Layout>
-                <Sider style={{ border: '1px solid rgba(38, 38, 38, 1)' }} width={200}>Sider</Sider>
+                <Sider style={{ border: '1px solid rgba(38, 38, 38, 1)' }} width={"20%"}>Sider</Sider>
                 <Content style={{ height: "100vh" }}>
-                    Hello
+                    <Row justify="space-around" align="middle">
+                        test
+                        <TextArea showCount maxLength={100} style={{ height: 120, width: "50%",  position: "absolute", top: 150, resize: "none" }} align="middle"/>
+                    </Row>
                 </Content>
-                {/* <Sider style={{ border: '1px solid rgba(38, 38, 38, 1)' }} width={400}>Sider</Sider> */}
+                <Sider style={{ border: '1px solid rgba(38, 38, 38, 1)' }} width={"15%"}>Sider</Sider>
             </Layout>
             <Footer style={{ textAlign: "center" }}>
                 StreamVerse©2022
